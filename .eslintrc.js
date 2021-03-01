188d66cfa724fd842aaaee01de63ae591211ee20
Ilya <ilyastarickov1996@gmail.com>
2021-03-01 17:35:57 +0400

    Fix Sanity check faulure

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,11 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    "jest/globals": true,
   },
   extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["jest"],
   rules: {},
 };
